a09408e88cca224b4c15dd33b09ccdb09c3ffe5f
spazer <lasercannon1@gmail.com>
2017-04-04 16:32:10 -0600

    Versioning

--- Match Detail Filler/MatchDetailFiller.Designer.cs
+++ Match Detail Filler/MatchDetailFiller.Designer.cs
@@ -568,7 +568,7 @@
             this.Controls.Add(this.tabControlType);
             this.MinimumSize = new System.Drawing.Size(562, 450);
             this.Name = "MatchDetailFiller";
-            this.Text = "Bracket Match Filler v2.4.0";
+            this.Text = "Bracket Match Filler v2.4.1";
             this.tabControlType.ResumeLayout(false);
             this.tabPageSingles.ResumeLayout(false);
             this.tabPageSingles.PerformLayout();
